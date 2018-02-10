83bbe5b5ab89a2937fdd73e6c8c6bfa753377128
Lorenzo <35295037+LorenzoElSalserito@users.noreply.github.com>
2018-02-10 13:09:50 +0100

    Update bubble_sort.py

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,11 +1,18 @@
 def bubbleSort(alist):
-    for passnum in range(len(alist)-1,0,-1):
+    """
+    sempre meglio creare una copia piuttosto che modificare valori per tramite di puntatori
+    altrimenti nell'esecuzione del programma risulta difficile comprendere in quale momento e posizione
+    all'interno del codice è avvenuta la sovrascrittura (eviti bug)
+    """
+    nlist = [a for a in alist]
+    for passnum in range(len(nlist)-1,0,-1):
         for i in range(passnum):
-            if alist[i]>alist[i+1]:
-                temp = alist[i]
-                alist[i] = alist[i+1]
-                alist[i+1] = temp
-    print(alist)
+            if nlist[i]>nlist[i+1]:
+                temp = nlist[i]
+                nlist[i] = nlist[i+1]
+                nlist[i+1] = temp
+    print(alist, ' -> ', nlist)
+    return nlist
 
 
 alist = [54,26,93,17,77,31,44,55,20]
